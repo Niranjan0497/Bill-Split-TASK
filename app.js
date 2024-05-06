5bd3516e217388291a03c2a3f9b532d9d83406a7
Niranjan0497 <niranjan0497@gmail.com>
2024-05-06 12:50:22 +0530

    Dom Task

---
+++ app.js
@@ -0,0 +1,43 @@
+function billSplit(){
+const inputvalue = document.getElementById('inputvalue').value;
+const noofpersons = document.getElementById('noofpersons').value;
+const centralGST = document.getElementById('centralGST')
+const stateGST = document.getElementById('stateGST')
+const discountOnBill=document.getElementById('discountOnBill').checked
+const outputValue = document.getElementById('outputValue')
+
+    if(inputvalue==="" || noofpersons===""){
+        alert("Enter a input fileds")
+    }else if(inputvalue <=0 || isNaN(inputvalue)){
+        alert("Enter Vaild Bill Amount")
+        // document.getElementById('inputvalue_alret').style.visibility = "visible"
+    }else if(noofpersons <=1 || isNaN(noofpersons)){
+        alert("Enter How Many Number of Persons in Number formate only")
+        // document.getElementById('noofpersons_alret').style.visibility = "visible"
+    }else if(inputvalue <= 100){
+        centralGST.innerHTML = "CGST: ZERO TAX";
+        stateGST.innerHTML = "SGST: ZERO TAX";
+        let originalOutput = Math.round(inputvalue / noofpersons);
+        if (discountOnBill) {
+            outputValue.value = Math.round(originalOutput * 0.95); // Applying 5% discount
+        } else {
+            outputValue.value = originalOutput;
+        }
+    }
+    else{
+        let cgst = inputvalue * 0.1;
+        centralGST.innerHTML = `CGST: ${cgst}`;
+        let sgst = inputvalue * 0.1;
+        stateGST.innerHTML = `SGST: ${sgst}`;
+        let originalOutput = Math.round(inputvalue / noofpersons);
+        if (discountOnBill) {
+            outputValue.value = Math.round(originalOutput * 0.95); // Applying 5% discount
+        } else {
+            outputValue.value = originalOutput;
+        }
+        
+    }
+    document.getElementById('inputvalue').value="";
+    document.getElementById('noofpersons').value="";
+
+}
